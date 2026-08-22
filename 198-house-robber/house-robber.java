742906d4fc5c51e4b8e0d1cb37f7d524512794ee
Anshulsingh-07 <kanhaiya20082204@gmail.com>
2026-08-23 01:17:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (57.30%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,13 @@
+class Solution {
+    int solve(int i,int nums[],int dp[]){
+        if(i>=nums.length)return 0;
+        if(dp[i]!=-1)return dp[i];
+        return dp[i] =  Math.max(nums[i]+solve(i+2,nums,dp),solve(i+1,nums,dp));
+    }
+
+    public int rob(int[] nums) {
+        int dp [] = new int[nums.length];
+        Arrays.fill(dp,-1);
+        return solve(0,nums,dp);
+    }
+}
